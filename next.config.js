b2be1eff220191b2d5d068194b21748f980a9787
hiiboo <130648472+hiiboo@users.noreply.github.com>
2023-10-27 12:16:31 +0900

    config変更

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-domains: ['via.placeholder.com', '127.0.0.1', 'localhost', 'yugo-glasgow.sakura.ne.jp'],
+domains: ['via.placeholder.com', '127.0.0.1', 'localhost', 'hibo.sakura.ne.jp'],
   },
 }
 
